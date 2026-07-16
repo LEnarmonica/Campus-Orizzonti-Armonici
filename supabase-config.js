e39c5eb331718e714264d874c01d076ee24f1f91
LEnarmonica <info@enarmonica.it>
2026-07-16 17:43:29 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.CAMPUS_SUPABASE = {
-  url: "https://zwmqzunphoopsvukewot.supabase.co/rest/v1/",
+  url: "https://zwmqzunphoopsvukewot.supabase.co",
   anonKey: "sb_publishable_QL_QcCks4WXjXNW3wYqExw_GuQzHftq"
 };
